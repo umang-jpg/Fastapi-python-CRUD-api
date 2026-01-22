fc9bd805b0080671c2b20d5f792b60b7f7bc1fb8
umang <umangpawar2005@gmail.com>
2026-01-22 19:15:38 +0530

    refactored the project to src layout and fixed the fastapi imports

---
+++ src/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from src.books.routes import book_router
+
+version = "v1"
+
+app = FastAPI(
+    title="Bookly",
+    description="A REST API for a book review web service",
+    version=version,
+)
+
+app.include_router(
+    book_router,
+    prefix=f"/api/{version}/books",
+    tags=["books"],
+)
